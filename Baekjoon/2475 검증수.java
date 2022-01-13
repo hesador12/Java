fa566b3eca44de1584cdbe4b4781f1bbd984020a
hesador12 <97518677+hesador12@users.noreply.github.com>
2022-01-13 23:41:14 +0900

    Create 2475 검증수.java

---
+++ Baekjoon/2475 검증수.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		
+		for(int i = 0; i < 5; i++ ) {
+			sum += Math.pow(sc.nextInt(), 2);
+		}
+		
+		sc.close();
+		
+		System.out.println( sum % 10);
+		
+	}
+}
