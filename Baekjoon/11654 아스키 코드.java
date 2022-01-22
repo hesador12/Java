c93c318325a61eb7aa16401579596a3536b7ce69
hesador12 <97518677+hesador12@users.noreply.github.com>
2022-01-23 02:50:27 +0900

    Create 11654 아스키 코드.java

---
+++ Baekjoon/11654 아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		char B = sc.nextLine().charAt(0);
+		int A = (int)B;
+		sc.close();
+    System.out.println(A);
+	}
+}
