bd3b70dc1352d7e27f0c566788b9aadae9d631ab
hesador12 <97518677+hesador12@users.noreply.github.com>
2022-01-25 14:18:26 +0900

    Update 1152 단어의 개수.java

--- Baekjoon/1152 단어의 개수.java
+++ Baekjoon/1152 단어의 개수.java
@@ -6,7 +6,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		String A = sc.nextLine();
 		StringTokenizer st = new StringTokenizer(A," ");
-		
+		//new StringTokenizer(A)라고만 해도 띄어쓰기가 구분자로 적용됨.. 
 		System.out.println(st.countTokens());
 	}
 }
