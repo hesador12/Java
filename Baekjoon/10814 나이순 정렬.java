aa986ee4108ae903bef5029698f4fe65c7a8b257
hesador12 <97518677+hesador12@users.noreply.github.com>
2022-01-29 08:25:05 +0900

    Create 10814 나이순 정렬.java

---
+++ Baekjoon/10814 나이순 정렬.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[][] arr = new String[N][2];
+
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.next();
+            arr[i][1] = sc.next();
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] age1, String[] age2) {
+                return Integer.parseInt(age1[0]) - Integer.parseInt(age2[0]);
+            }
+     });
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+    }
+  }
+}
